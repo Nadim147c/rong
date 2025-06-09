59c4eb3d81657aea4d794892e10ee0e2cedcd817
Ephemeral <theephemeral.txt@gmail.com>
2025-06-10 00:42:05 +0600

    feat(templates): add json function for templates

--- templates/func.go
+++ templates/func.go
@@ -1,6 +1,7 @@
 package templates
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
 	"text/template"
@@ -10,8 +11,17 @@ var funcs = template.FuncMap{
 	"upper": strings.ToUpper,
 	"lower": strings.ToLower,
 	"qoute": qoute,
+	"json":  jsonString,
 }
 
 func qoute(s any) string {
 	return fmt.Sprintf("%q", s)
+}
+
+func jsonString(s any) string {
+	bytes, err := json.Marshal(s)
+	if err != nil {
+		return "null"
+	}
+	return string(bytes)
 }
