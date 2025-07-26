f390db353f2f63ef91973986831d36ed3d88bb46
Ephemeral <theephemeral.txt@gmail.com>
2025-07-26 12:55:18 +0600

    feat(cli): add colored help command

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/Nadim147c/rong/cmd"
+import (
+	"github.com/Nadim147c/rong/cmd"
+	cc "github.com/ivanpirog/coloredcobra"
+)
 
 func main() {
+	cc.Init(&cc.Config{
+		RootCmd:         cmd.Command,
+		Headings:        cc.Cyan + cc.Bold + cc.Underline,
+		Commands:        cc.Yellow + cc.Bold,
+		CmdShortDescr:   cc.Bold,
+		Example:         cc.Italic,
+		ExecName:        cc.Bold,
+		Flags:           cc.Green + cc.Bold,
+		FlagsDataType:   cc.Red + cc.Bold,
+		NoExtraNewlines: true,
+	})
+
 	cmd.Command.Execute()
 }
