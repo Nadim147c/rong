9905b44cd8da51267a71360e4ee857dc2b800a29
Ephemeral <theephemeral.txt@gmail.com>
2025-09-06 20:51:32 +0600

    fix: add --json flage for color command

--- cmd/color/cmd.go
+++ cmd/color/cmd.go
@@ -27,6 +27,7 @@ func init() {
 	Command.Flags().Float64("contrast", 0.0, "contrast adjustment (-1.0 to 1.0)")
 	Command.Flags().String("platform", string(dynamic.Phone), "target platform (phone or watch)")
 	Command.Flags().Int("version", int(dynamic.V2021), "version of the theme (2021 or 2025)")
+	Command.Flags().BoolP("json", "j", false, "print generated colors as json")
 	Command.Flags().Bool("dry-run", false, "generate colors without applying templates")
 }
 
