b38c73d02ec34303b82ea1a749a3f023f9790361
Ephemeral <theephemeral.txt@gmail.com>
2026-01-09 01:17:13 +0600

    fix(config): change base16 default colors

--- internal/config/config.go
+++ internal/config/config.go
@@ -57,12 +57,12 @@ var (
 		"", "base16.method", enums.Base16MethodStatic, "Base16 color generation method",
 		enums.Base16MethodNames(), enums.ParseBase16Method,
 	)
-	Base16Black   = newColorOption("", "base16.colors.black", "#FF0000", "Base16 black source color")
+	Base16Black   = newColorOption("", "base16.colors.black", "#222222", "Base16 black source color")
 	Base16Blue    = newColorOption("", "base16.colors.blue", "#0000FF", "Base16 blue source color")
 	Base16Cyan    = newColorOption("", "base16.colors.cyan", "#5555FF", "Base16 cyan source color")
 	Base16Green   = newColorOption("", "base16.colors.green", "#00FF00", "Base16 green source color")
 	Base16Magenta = newColorOption("", "base16.colors.magenta", "#FF00FF", "Base16 magenta source color")
-	Base16Red     = newColorOption("", "base16.colors.red", "#222222", "Base16 red source color")
+	Base16Red     = newColorOption("", "base16.colors.red", "#FF0000", "Base16 red source color")
 	Base16White   = newColorOption("", "base16.colors.white", "#EEEEEE", "Base16 white source color")
 	Base16Yellow  = newColorOption("", "base16.colors.yellow", "#FFFF00", "Base16 yellow source color")
 )
